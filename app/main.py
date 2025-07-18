5ff5382e79c3705a6472c7bb23bab30448bda2ec
pridomtecnologia <suporte@pridom.com.br>
2025-07-17 22:42:54 -0300

    cadastro cliente

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from app.api.usuario import usuario_api
-from app.api.customer import customer_api 
+from app.api.customer import customer_api
+
 app = FastAPI()
 
 app.include_router(usuario_api.router)
